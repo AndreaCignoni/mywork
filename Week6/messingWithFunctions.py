35417dbcde0d43cbd23cdd5d5ae20ad638bc7405
unknown <andreacignoni@hotmail.com>
2023-02-27 22:23:43 +0000

    task6

---
+++ Week6/messingWithFunctions.py
@@ -0,0 +1,18 @@
+# Messing With Functions
+# How defining and using functions
+# Author: Andrea Cignoni
+
+# x , y , z = (1, 2, 3)
+# print(x, y, z, sep="", end="")
+# print(f"{x}-{y} {z}")
+# print("{} {}--{}".format(x, y, z))
+
+def cube(number):
+    # print(number)
+    return (number)
+
+#ans = cube(23)
+# print("we got {ans}")
+num = 45
+
+print(f"and {num} is {cube, 2}")
