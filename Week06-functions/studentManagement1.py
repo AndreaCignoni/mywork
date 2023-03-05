46dceae4d3488355eabf90d751038d20ccd328f9
unknown <andreacignoni@hotmail.com>
2023-03-05 22:39:07 +0000

    task6

--- Week06-functions/studentManagement1.py
+++ Week06-functions/studentManagement1.py
@@ -5,7 +5,6 @@
 # called doView()
 # Author: Andrea Cignoni
 
-students = []
 def displayMenu():
     print("What would you like to do?")
     print("\t(a) Add new student")
@@ -14,48 +13,46 @@ def displayMenu():
     choice = input("Type one letter (a/v/q)").strip()
     return choice
 
-def getModules():
-    modules = []
-    modulesname = input ["please enter  the module name (blank to quit)"]
-    while (modulesname != ""):
-        module = {}
-        module["name"]= modulesname
-        module["grade"] = int(input("enter grade"))
-        modules.append(module)
-
-        modulesname = input("please enter the module name")
-
-    return modules
-
 def doAdd(students):
     currentStudent = {}
     currentStudent["name"]= input("please enter name: ")
     currentStudent["modules"]= readModules ()
-
     students.append(currentStudent)
+
+def readModules():
+    modules = []
+    modulesname = input ("Please enter  the first Module name (blank to quit):").strip()
     
-def doView(students):
-    for student in students:
-        print (student["name"])
-        for module in student:
-            print("\t", module["name"], ":", module["grade"]) 
+    while (modulesname != ""):
+        module = {}
+        module["name"]= modulesname
+        module["grade"] = int(input("\t\Enter grade"))
+        modules.append(module)
+
+        modulesname = input("\tPlease enter the module name").strip()
+
+    return modules
+
+def displayModules (modules):
+    print("\tName   \tGrade")
+    for module in modules:
+        print (f"\t{ module["name"]} \t{ module["grade"]}")
+
+def doView (students):
+    for currentStudent in students:
+        print (currentStudent["name"])
+        displayModules (currentStudent ["modules"])
     
 # main program
+students = []
 choice = displayMenu()
 while (choice != 'q'):
-    # we could do this with labda functions
+    # we could do this with lambda functions
     # this kept basic on purpose
     if choice == 'a':
-        doAdd()
+        doAdd(students)
     elif choice == 'v':
-        doView()
+        doView(students)
     elif choice !='q':
         print("\n\nplease select either a,v or q")
     choice=displayMenu ()
-def readModules():
-    return[]
-
-
-#test
-
-print(students)
