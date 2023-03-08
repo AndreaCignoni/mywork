ccd20983be40b1003a8122bdc88fcc40a572e769
unknown <andreacignoni@hotmail.com>
2023-03-08 18:45:15 +0000

    task6

--- Week06-functions/studentManagement1.py
+++ Week06-functions/studentManagement1.py
@@ -36,7 +36,7 @@ def readModules():
 def displayModules (modules):
     print("\tName   \tGrade")
     for module in modules:
-        print (f"\t{ module["name"]} \t{ module["grade"]}")
+        print (f"\t{ module['name']} \t{ module['grade']}")
 
 def doView (students):
     for currentStudent in students:
